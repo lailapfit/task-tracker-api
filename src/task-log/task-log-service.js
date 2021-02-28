70f73bace58fc2acb302e27cfb32a35a084288ad
Laila Fitriana <lailafitriana@me.com>
2021-02-28 18:50:44 -0500

    Updated queries with joins

--- src/task-log/task-log-service.js
+++ src/task-log/task-log-service.js
@@ -1,15 +1,38 @@
+//.join('contacts', 'users.id', '=', 'contacts.user_id')
+
+
 const TaskLogService = {
     getAllTaskLogs(knex) {
-        return knex.select('*').from('task_logs');
+        return knex.select(knex.raw('task_logs.id AS task_log_id, task_logs.task_id, tasks.description AS task_description, users.id AS user_id, users.email AS user_email, task_logs.duration_minutes, tasks.project_id, projects.name AS project_name'))
+        .from('task_logs')
+        .innerJoin('users', 'users.id', '=', 'task_logs.user_id')
+        .innerJoin('tasks', 'tasks.id', '=', 'task_logs.task_id')
+        .innerJoin('projects', 'tasks.project_id', '=', 'projects.id')
     },
     getTaskLogById(knex, id) {
-        return knex.select('*').from('task_logs').first().where({ id: `${id}`});
+        return knex.select(knex.raw('task_logs.id AS task_log_id, task_logs.task_id, tasks.description AS task_description, users.id AS user_id, users.email AS user_email, task_logs.duration_minutes, tasks.project_id, projects.name AS project_name'))
+        .from('task_logs')
+        .first()
+        .innerJoin('users', 'users.id', '=', 'task_logs.user_id')
+        .innerJoin('tasks', 'tasks.id', '=', 'task_logs.task_id')
+        .innerJoin('projects', 'tasks.project_id', '=', 'projects.id')
+        .where('task_logs.id', '=', id);
     },
     getTaskLogsByTaskId(knex, taskId) {
-        return knex.select('*').from('task_logs').where({ task_id: `${taskId}`});
+        return knex.select(knex.raw('task_logs.id AS task_log_id, task_logs.task_id, tasks.description AS task_description, users.id AS user_id, users.email AS user_email, task_logs.duration_minutes, tasks.project_id, projects.name AS project_name'))
+        .from('task_logs')
+        .innerJoin('users', 'users.id', '=', 'task_logs.user_id')
+        .innerJoin('tasks', 'tasks.id', '=', 'task_logs.task_id')
+        .innerJoin('projects', 'tasks.project_id', '=', 'projects.id')
+        .where('task_id', '=', taskId);
     },
     getTaskLogsByUserId(knex, userId) {
-        return knex.select('*').from('task_logs').where({ user_id: `${userId}`});
+        return knex.select(knex.raw('task_logs.id AS task_log_id, task_logs.task_id, tasks.description AS task_description, users.id AS user_id, users.email AS user_email, task_logs.duration_minutes, tasks.project_id, projects.name AS project_name'))
+        .from('task_logs')
+        .innerJoin('users', 'users.id', '=', 'task_logs.user_id')
+        .innerJoin('tasks', 'tasks.id', '=', 'task_logs.task_id')
+        .innerJoin('projects', 'tasks.project_id', '=', 'projects.id')
+        .where('user_id', '=', userId);
     },
     createTaskLog(knex, task_id, user_id, duration_minutes) {
         return knex('task_logs').returning('id').insert({ task_id: `${task_id}`, user_id: `${user_id}`, duration_minutes: `${duration_minutes}`});
