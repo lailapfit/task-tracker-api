9c52e4a344521d7df311673e76e17785c3a3459b
Laila Fitriana <lailafitriana@me.com>
2021-02-28 17:41:49 -0500

    Add task log service

---
+++ src/task-log/task-log-service.js
@@ -0,0 +1,25 @@
+const TaskLogService = {
+    getAllTaskLogs(knex) {
+        return knex.select('*').from('task_logs');
+    },
+    getTaskById(knex, id) {
+        return knex.select('*').from('task_logs').first().where({ id: `${id}`});
+    },
+    getTaskLogsByProjectId(knex, projectId) {
+        return knex.select('*').from('task_logs').where({ project_id: `${projectId}`});
+    },
+    getTaskLogsByUserId(knex, userId) {
+        return knex.select('*').from('task_logs').where({ user_id: `${userId}`});
+    },
+    createTaskLog(knex, task_id, user_id, duration_minutes) {
+        return knex('task_logs').returning('id').insert({ task_id: `${task_id}`, user_id: `${user_id}`, duration_minutes: `${duration_minutes}`});
+    },
+    updateTaskLogById(knex, id, data) {
+        return knex('task_logs').where({ id: `${id}`}).update({ task_id: `${data.task_id}`, user_id: `${data.user_id}`, duration_minutes: `${data.duration_minutes}`}, ['id', 'task_id', 'user_id', 'duration_minutes']);
+    },
+    deleteTaskLogById(knex, id) {
+        return knex('task_logs').where({ id: `${id}`}).del();
+    }
+}
+
+module.exports = TaskLogService;
