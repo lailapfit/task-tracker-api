4274d8637d4a28b9ce3efe287fcf0ff87f9f6598
Laila Fitriana <lailafitriana@me.com>
2021-02-28 16:30:39 -0500

    Add customer add, update, delete queries

---
+++ src/customer/customer-service.js
@@ -0,0 +1,25 @@
+const CustomerService = {
+    getCustomerByName(knex, name) {
+        return knex.select('*').from('customers').first().where({ name: `${name}`});
+    },
+    getCustomerById(knex, id) {
+        return knex.select('*').from('customers').first().where({ id: `${id}`});
+    },
+    createCustomer(knex, name) {
+        return knex('customers').returning('id').insert({ name: `${name}`});
+    },
+    updateCustomerByName(knex, name, data) {
+        return knex('customers').where({ name: `${name}`}).update({ name: `${data}`}, ['id', 'name']);
+    },
+    updateCustomerById(knex, id, data) {
+        return knex('customers').where({ id: `${id}`}).update({ name: `${data}`}, ['id', 'name']);
+    },
+    deleteCustomerByName(knex, name) {
+        return knex('customers').where({ name: `${name}`}).del();
+    },
+    deleteCustomerById(knex, id) {
+        return knex('customers').where({ id: `${id}`}).del();
+    }
+}
+
+module.exports = CustomerService;
