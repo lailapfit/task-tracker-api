4a331fad3305114a2f8f4fe4b3c896462a4e46a0
Laila Fitriana <lailafitriana@me.com>
2021-02-28 21:27:55 -0500

    Updated task services

--- src/task/task-router.js
+++ src/task/task-router.js
@@ -13,6 +13,20 @@ taskRouter
         return res.status(500).json({error: {message: err, detail: 'getAllTasks'}});
     })
 })
+.post((req, res) => {
+    Task.createTask(req.app.get('db'), req.body)
+    .then(task => {
+        if (!task) {
+            return res.status(404).json({
+                error: {message: 'Task cannot be created'}
+            });
+        }
+        res.json({ task_id: task });
+    })
+    .catch(err => {
+        return res.status(500).json({error: {message: err, detail: 'createTask'}});
+    })
+})
 
 taskRouter
 .route('/task-id/:taskid')
@@ -46,7 +60,7 @@ taskRouter
     })
 })
 .delete((req, res) => {
-    Task.deleteTaskByTaskId(req.app.get('db'), req.params.id)
+    Task.deleteTaskByTaskId(req.app.get('db'), req.params.taskid)
     .then(task => {
         if (!task) {
             return res.status(404).json({
@@ -77,34 +91,6 @@ taskRouter
         return res.status(500).json({error: {message: err, detail: 'getTaskByProjectId'}});
     })
 })
-.put((req, res) => {
-    Task.updateTaskDescriptionByProjectId(req.app.get('db'), req.params.projectid, req.body)
-    .then(task => {
-        if (!task) {
-            return res.status(404).json({
-                error: {message: 'Task cannot be updated'}
-            });
-        }
-        res.json(task);
-    })
-    .catch(err => {
-        return res.status(500).json({error: {message: err, detail: 'updateTaskDescriptionByProjectId'}});
-    })
-})
-.delete((req, res) => {
-    Task.deleteTaskByProjectId(req.app.get('db'), req.params.projectid)
-    .then(task => {
-        if (!task) {
-            return res.status(404).json({
-                error: {message: 'Task cannot be updated'}
-            });
-        }
-        res.status(200).send('Task successfully deleted');
-    })
-    .catch(err => {
-        return res.status(500).json({error: {message: err, detail: 'deleteTaskByProjectId'}});
-    })
-})
 
 taskRouter
 .route('/description/:description')
@@ -121,20 +107,6 @@ taskRouter
     })
     .catch(err => {
         return res.status(500).json({error: {message: err, detail: 'getTaskByDescription'}});
-    })
-})
-.put((req, res) => {
-    Task.updateTaskDescriptionById(req.app.get('db'), req.params.description, req.body)
-    .then(task => {
-        if (!task) {
-            return res.status(404).json({
-                error: {message: 'Task cannot be updated'}
-            });
-        }
-        res.json(task);
-    })
-    .catch(err => {
-        return res.status(500).json({error: {message: err, detail: 'updateTaskDescriptionById'}});
     })
 })
 
--- src/task/task-service.js
+++ src/task/task-service.js
@@ -27,23 +27,14 @@ const TaskService = {
         .innerJoin('customers', 'customers.id', '=', 'projects.customer_id')
         .where('tasks.project_id','=', projectId);
     },
-    createTask(knex, description, project_id) {
-        return knex('tasks').returning('id').insert({ description: `${description}`, project_id: `${project_id}`});
-    },
-    updateTaskDescriptionById(knex, id, data) {
-        return knex('tasks').where({ id: `${id}`}).update({ description: `${data}`}, ['id', 'description', 'project_id']);
-    },
-    updateTaskDescriptionByProjectId(knex, projectId, data) {
-        return knex('tasks').where({ project_id: `${projectId}`}).update({ description: `${data}`}, ['id', 'description', 'project_id']);
+    createTask(knex, data) {
+        return knex('tasks').returning('id').insert(data);
     },
     updateTaskProjectByTaskId(knex, taskId, data) {
-        return knex('tasks').where({ id: `${taskId}`}).update({ project_id: `${data}`}, ['id', 'description', 'project_id']);
+        return knex('tasks').where({ id: `${taskId}`}).update(data, ['id', 'description', 'project_id']);
     },
     deleteTaskByTaskId(knex, taskId) {
         return knex('tasks').where({ id: `${taskId}`}).del();
-    },
-    deleteTaskByProjectId(knex, projectId) {
-        return knex('tasks').where({ project_id: `${projectId}`}).del();
     }
 }
 
