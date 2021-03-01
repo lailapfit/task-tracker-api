db7c4ae267b5256d0697725ff525a40e0ae03cd0
Laila Fitriana <lailafitriana@me.com>
2021-02-28 19:06:26 -0500

    Updated queries with joins for get

--- src/task/task-service.js
+++ src/task/task-service.js
@@ -1,15 +1,31 @@
 const TaskService = {
     getAllTasks(knex) {
-        return knex.select('*').from('tasks');
+        return knex.select(knex.raw('tasks.id AS task_id, tasks.description AS task_description, tasks.project_id, projects.name AS project_name, projects.customer_id, customers.name AS customer_name'))
+        .from('tasks')
+        .innerJoin('projects', 'tasks.project_id', '=', 'projects.id')
+        .innerJoin('customers', 'customers.id', '=', 'projects.customer_id');
     },
     getTaskByDescription(knex, description) {
-        return knex.select('*').from('tasks').where('description', 'like', `%${description}%`);
+        return knex.select(knex.raw('tasks.id AS task_id, tasks.description AS task_description, tasks.project_id, projects.name AS project_name, projects.customer_id, customers.name AS customer_name'))
+        .from('tasks')
+        .innerJoin('projects', 'tasks.project_id', '=', 'projects.id')
+        .innerJoin('customers', 'customers.id', '=', 'projects.customer_id')
+        .where('tasks.description', 'like', `%${description}%`);
     },
     getTaskById(knex, id) {
-        return knex.select('*').from('tasks').first().where({ id: `${id}`});
+        return knex.select(knex.raw('tasks.id AS task_id, tasks.description AS task_description, tasks.project_id, projects.name AS project_name, projects.customer_id, customers.name AS customer_name'))
+        .from('tasks')
+        .innerJoin('projects', 'tasks.project_id', '=', 'projects.id')
+        .innerJoin('customers', 'customers.id', '=', 'projects.customer_id')
+        .first()
+        .where('tasks.id', '=', id);
     },
     getTaskByProjectId(knex, projectId) {
-        return knex.select('*').from('tasks').where({ project_id: `${projectId}`});
+        return knex.select(knex.raw('tasks.id AS task_id, tasks.description AS task_description, tasks.project_id, projects.name AS project_name, projects.customer_id, customers.name AS customer_name'))
+        .from('tasks')
+        .innerJoin('projects', 'tasks.project_id', '=', 'projects.id')
+        .innerJoin('customers', 'customers.id', '=', 'projects.customer_id')
+        .where('tasks.project_id','=', projectId);
     },
     createTask(knex, description, project_id) {
         return knex('tasks').returning('id').insert({ description: `${description}`, project_id: `${project_id}`});
