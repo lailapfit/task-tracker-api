b3eb63200b06dd40254b4b4baf25dfaad8fb35f7
Laila Fitriana <lailafitriana@me.com>
2021-03-01 20:26:36 -0500

    Updated keys

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT,
-    DATABASE_URL: 'postgres://localhost:5432/postgres',
-    SECRET: '1zei3Ng0i9V8CrLMlmGxejAiBaRzfBjuW8yDSl2a1wd3dNHheiNkpvXnyjfWIy5'
+    DATABASE_URL: process.env.DATABASE_URL,
+    SECRET: process.env.SECRET
 }
